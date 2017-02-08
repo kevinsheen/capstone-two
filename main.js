a7a627fd7e7cf0054c1ca7adca2309a14897ca09
kevinjsheen <kevinjsheen@gmail.com>
2017-02-08 02:27:40 -0500

    edited js

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ $( document ).ready(function() {
         total: 0
     };
 	
-	$('.vote').on('click', function(event){ // set up .vote class 
+	$('.vote').on('click', function(event){ 
 	   if ($(this).attr('data-vote') === 'great'){
 		   ++voteCounts.great; 
 		   ++voteCounts.total;
 		   console.log(voteCounts);
 		   } 
-		else {						        // (($(this).attr('data-vote') === 'greatest')){??
+		else {						        
 		   ++voteCounts.greatest;
 	     ++voteCounts.total;
 		   console.log(voteCounts);
 	   }
-	var greatPercent = ((voteCounts.great/voteCounts.total) * 100 + '%');  // determine percentages on progress bar for great
+	var greatPercent = ((voteCounts.great/voteCounts.total) * 100 + '%');  
 	console.log(greatPercent);
-	$('.great-progress').css('width', greatPercent);						//used .css over .attr
-	var greatestPercent = ((voteCounts.greatest/voteCounts.total) * 100 + '%');  // determine percentage on progress bar for greatest
+	$('.great-progress').css('width', greatPercent);					
+	var greatestPercent = ((voteCounts.greatest/voteCounts.total) * 100 + '%');  
 	console.log(greatestPercent); 
 	$('.greatest-progress').css('width', greatestPercent); 
 		
